0203b3bd63d97e79e58f93309c340da7159dad92
Leonid578 <Leonid.Ivanov187@gmail.com>
2022-01-09 18:32:05 +0200

    upd file

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector ('body');
+let timerId;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  startBtn.addEventListener('click', onStartClick);
+  stopBtn.addEventListener('click', onStopClick);
+
+  function onStartClick(){
+    timerId = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`
+      }, 1000); 
+      startBtn.disabled = 'disabled';
+      stopBtn.disabled = '';
+      console.log('start')
+  }
+
+  function onStopClick(){
+    startBtn.disabled = '';
+    stopBtn.disabled = 'disabled';
+    clearInterval(timerId)
+    console.log('stop')
+  };
